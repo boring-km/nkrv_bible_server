5120807287fb37cb7032530fedf2d193fb4982fd
boring-km <kms0644804@naver.com>
2021-08-07 23:19:35 +0900

    [master] 시편 조회 시 1편 0절 내용 나오는 버그 수정

--- db/search_service.js
+++ db/search_service.js
@@ -2,7 +2,7 @@ const db = require('./load');
 
 const searchOne = (label, chapter, paragraph) => new Promise((resolve) => {
     db((connection) => {
-        const query = `select long_label, chapter, paragraph, sentence from bible2 where long_label = '${label}' and chapter = ${chapter} and paragraph = ${paragraph};`;
+        const query = `select long_label, chapter, paragraph, sentence from bible2 where long_label = '${label}' and chapter = ${chapter} and paragraph = ${paragraph} and paragraph > 0;`;
         console.log(query);
         connection.query(query, (err, results) => {
             if (err) {
@@ -17,7 +17,7 @@ const searchOne = (label, chapter, paragraph) => new Promise((resolve) => {
 
 const searchMultiLines = (label, chapter, start, end) => new Promise((resolve) => {
     db((connection) => {
-        const query = `select long_label, chapter, paragraph, sentence from bible2 where long_label = '${label}' and chapter = ${chapter} and paragraph between ${start} and ${end};`;
+        const query = `select long_label, chapter, paragraph, sentence from bible2 where long_label = '${label}' and chapter = ${chapter} and paragraph between ${start} and ${end} and paragraph > 0;`;
         console.log(query);
         connection.query(query, (err, results) => {
             if (err) {
@@ -32,7 +32,7 @@ const searchMultiLines = (label, chapter, start, end) => new Promise((resolve) =
 
 const searchChapter = (label, chapter) => new Promise((resolve) => {
     db((connection) => {
-        const query = `select long_label, chapter, paragraph, sentence from bible2 where long_label = '${label}' and chapter = ${chapter};`;
+        const query = `select long_label, chapter, paragraph, sentence from bible2 where long_label = '${label}' and chapter = ${chapter} and paragraph > 0;`;
         console.log(query);
         connection.query(query, (err, results) => {
             if (err) {
